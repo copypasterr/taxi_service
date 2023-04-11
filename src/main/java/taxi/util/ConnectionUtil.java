55931bdd78334b185faef498936f0a990a552066
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-04-11 03:14:51 +0300

    Readme

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,11 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://ui3p9ntit1u51gte:"
-            + "UGhsouQzcvKmtPjBlflO@blaujqesoflkbrjm5p5b-mysql"
-            + ".services.clever-cloud.com:3306/blaujqesoflkbrjm5p5b";
-    private static final String USERNAME = "ui3p9ntit1u51gte";
-    private static final String PASSWORD = "UGhsouQzcvKmtPjBlflO";
+    private static final String URL = "jdbc:mysql://YOUR_URL:3306";
+    private static final String USERNAME = "USERNAME";
+    private static final String PASSWORD = "PASSWORD";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
